fa4422d4ab132ef1a5e0be10a82c4d4e3147f3ab
colonel51 <67343608+colonel51@users.noreply.github.com>
2023-03-10 18:33:18 +0300

    Add files via upload

---
+++ odev1.py
@@ -0,0 +1,35 @@
+#Str -> String veri tipi
+#int -> Tam sayılar için veri tipi
+#float -> ondalıklı sayılar için veri tipi
+#list , tuple -> sıralama için 
+#dict -> haritalama için
+#set -> set veri tipi
+#bool -> boolean veri tipi
+#bytes , bytearray -> binary için veri tipi
+
+# Kodlama.io sitesinde String veri tipi olarak sitedeki tüm yazıları sayabilir.
+# Numerik veri tipleri için kurs bitirme yüzdesi , tarihler sayılabilir.
+# Boolean için bitir ve devam et , kullanıcı girişi örnek olarak verilebilir.
+
+# Kodlama.io sitesinde kullanılan şart blokları için kullanıcı girişi ve ödev bitirme tuşu örnek olarak verilebilir.
+# Bitir ve devam et butonuna bastığımızda kurs tamamlanma yüzdesi artmaktadır.
+# Kurs içeriğini görmek için kayıt olmak şart bloklarına örnektir.
+
+
+
+admin = True
+password = True
+
+if admin==True and password==True:
+    print("Giriş başarılı.")
+else:
+    print("Giriş başarısız.")
+     
+     
+     
+enrolled = True
+
+if enrolled==True:
+    print("Kursa kayıt oldunuz.")
+else:
+    print("İçeriği görmek için kursa kayıt olunuz.")
